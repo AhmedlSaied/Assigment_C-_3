2636de21ccf3382a98ada38344e1d4dcd3333b68
Ahmed Mohamed saeed <ahmed_m16709@cic-cairo.com>
2026-04-27 03:56:24 +0300

    Question-3

--- Program.cs
+++ Program.cs
@@ -18,3 +18,12 @@ Console.WriteLine(d2);
 this code will print "2" because n / 2 is an integer division, resulting in 2. To get the correct result as a double, you can cast n to double: double d2 = (double)n / 2;
 */
 #endregion
+#region Q3
+/*Q3: You read a number from user input .. Write the correct line to
+get age as int.
+*/
+Console.WriteLine("Enter your age:");
+int age =int.Parse(Console.ReadLine());
+// This code will read a line of input from the user, parse it as an integer, and store it in the variable 'age'. If the user enters a valid integer, it will be assigned to 'age'.
+//If the input is not a valid integer, it will throw a FormatException.
+#endregion
