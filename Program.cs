769448583785b0a36d93d461f359209cbd34b316
Ahmed Mohamed saeed <ahmed_m16709@cic-cairo.com>
2026-04-27 03:44:18 +0300

    Question-2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿//Q1: What will this print and explain what happens?
+﻿
+#region Q1
+//Q1: What will this print and explain what happens?
 
 double d = 9.99;
 int x = (int)d;
@@ -6,3 +8,13 @@ Console.WriteLine(x);
 /*
 this code will print "9" becuse when converting double to int ,the descimal wiill be removed and only the number before the dcimal point will be kept
 */
+#endregion
+#region Q2
+//Q2: This code doesn’t compile. Fix it with the smallest change?
+int n = 5;
+double d2 =(double) n / 2;
+Console.WriteLine(d2);
+/*
+this code will print "2" because n / 2 is an integer division, resulting in 2. To get the correct result as a double, you can cast n to double: double d2 = (double)n / 2;
+*/
+#endregion
