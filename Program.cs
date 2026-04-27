17dca6b5a3f82987cd3cfbec8c9eb4cad953e2b7
Ahmed Mohamed saeed <ahmed_m16709@cic-cairo.com>
2026-04-27 03:35:29 +0300

    Question-1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Q1: What will this print and explain what happens?
+
+double d = 9.99;
+int x = (int)d;
+Console.WriteLine(x);
+/*
+this code will print "9" becuse when converting double to int ,the descimal wiill be removed and only the number before the dcimal point will be kept
+*/
