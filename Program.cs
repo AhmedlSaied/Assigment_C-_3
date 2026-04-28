f978318b066e2b4d4087ba9a2c56012c70e09d48
Ahmed Mohamed saeed <ahmed_m16709@cic-cairo.com>
2026-04-28 08:08:04 +0300

    question-8

--- Program.cs
+++ Program.cs
@@ -4,18 +4,18 @@
 
 using System.Numerics;
 
-double d = 9.99;
-int x = (int)d;
-Console.WriteLine(x);
+//double d = 9.99;
+//int x = (int)d;
+//Console.WriteLine(x);
 /*
 this code will print "9" becuse when converting double to int ,the descimal wiill be removed and only the number before the dcimal point will be kept
 */
 #endregion
 #region Q2
 //Q2: This code doesn’t compile. Fix it with the smallest change?
-int n = 5;
-double d2 =(double) n / 2;
-Console.WriteLine(d2);
+//int n = 5;
+//double d2 =(double) n / 2;
+//Console.WriteLine(d2);
 /*
 this code will print "2" because n / 2 is an integer division, resulting in 2. To get the correct result as a double, you can cast n to double: double d2 = (double)n / 2;
 */
@@ -24,8 +24,8 @@ this code will print "2" because n / 2 is an integer division, resulting in 2. T
 /*Q3: You read a number from user input .. Write the correct line to
 get age as int.
 */
-Console.WriteLine("Enter your age:");
-int age =int.Parse(Console.ReadLine());
+//Console.WriteLine("Enter your age:");
+//int age =int.Parse(Console.ReadLine());
 // This code will read a line of input from the user, parse it as an integer, and store it in the variable 'age'. If the user enters a valid integer, it will be assigned to 'age'.
 //If the input is not a valid integer, it will throw a FormatException.
 #endregion
@@ -37,11 +37,39 @@ Console.WriteLine(x);
 */
 //this code will not be run because the string cannot be converted or cast to an integer
 #endregion
+#region Q5
 //Q5: Complete the code from the previous question so it prints
 //Invalid if conversion into int fails, otherwise prints the number
-string s = "12a";
+/*string s = "12a";
 if (int.TryParse(s, out x))
 
     Console.WriteLine(x);
 else
 Console.WriteLine("Invalid input");
+*/
+#endregion
+#region Q6
+//Q6: What will this print and explain why ?
+//object o = 10;
+//int a = (int)o;
+//Console.WriteLine(a + 1);
+// This code will print "11" because the object 'o' is assigned the value 10, which is an integer. When we cast 'o' to an integer and assign it to 'a', 
+//it successfully converts the value to 10. Then, when we add 1 to 'a', it results in 11, which is printed to the console.
+#endregion
+#region Q7
+//Q7: What will this print and explain why and if there is a
+//problem handle it ?
+//object o = 10;
+//long x = (long)(int)o;
+//Console.WriteLine(x);
+// This code will throw an InvalidCastException at runtime because you cannot directly cast an object that contains an integer (10) to a long type.
+#endregion
+#region Q8
+//Q8: Fix this to avoid exceptions and print -1 if conversion isn’t
+//possible?
+object o = 10;
+int? temp = o as int?;
+long x = temp ?? -1; 
+
+Console.WriteLine(x);
+#endregion
